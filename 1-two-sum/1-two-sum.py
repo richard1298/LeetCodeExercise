cf940d5c044c6ff638b584f2aef39d4f33cb8848
richard1298 <richardhxw1298@gmail.com>
2022-07-06 00:33:02 -0400

    Time: 82 ms (75.26%), Space: 15.1 MB (50.23%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #The idea is to use hashmap for a faster search; drawback is extra memory
-        #Algorithm: create a dictionary from the first elem up till the previous elem;if target - current elem exists in the dictionary, return; otherwise expand the dictionary
-        search_dict = {}
-        for index, val in enumerate(nums):
-            if target - val in search_dict:
-                return [search_dict[target - val],index]
+        dic = {nums[0]:0}
+        for i in range(1,len(nums)):
+            if target - nums[i] in dic:
+                return [dic[target - nums[i]], i]
             else:
-                search_dict[val] = index
-                
-            
+                dic[nums[i]] = i
+        
