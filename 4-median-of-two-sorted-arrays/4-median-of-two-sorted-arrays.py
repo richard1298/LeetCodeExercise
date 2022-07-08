aa653fcfa0c2c3c86513a55b689f6bb34f3da885
richard1298 <richardhxw1298@gmail.com>
2022-07-07 23:27:45 -0400

    Time: 129 ms (63.50%), Space: 14.3 MB (25.50%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,29 +1,32 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        shortlist, longlist = nums1, nums2
-        total = len(nums1) + len(nums2)    
-        #define the pivot index for the short array
-        half = total//2
-        if len(longlist) < len(shortlist):
-            shortlist, longlist = longlist, shortlist
-        l = 0
-        r = len(shortlist) - 1
-        while True:
-            #get the partition of the short array 
-            i = (l + r)//2 
-            j = half - i - 2
-            shortlist_left = shortlist[i] if i >=0 else -float('inf')
-            shortlist_right = shortlist[i+1] if (i+1)<len(shortlist) else float('inf')
-            longlist_left = longlist[j] if j >=0 else -float('inf')
-            longlist_right = longlist[j+1] if (j+1)<len(longlist) else float('inf')
-            #define four variables for easy reference:
-            if shortlist_left<=longlist_right and longlist_left <= shortlist_right:
-                if total%2:
-                    return min(shortlist_right,longlist_right)
+        if len(nums1) ==0:
+            return nums2[len(nums2)//2] if len(nums2)%2 == 1 else 0.5*(nums2[len(nums2)//2]+nums2[len(nums2)//2-1])
+        elif len(nums2) == 0:
+            return nums1[len(nums1) // 2] if len(nums1) % 2 == 1 else 0.5 * (
+                        nums1[len(nums1) // 2] + nums1[len(nums1) // 2 - 1])
+        else:
+            if len(nums1) > len(nums2):
+                nums1, nums2 = nums2, nums1
+            l, r = 0, len(nums1) - 1
+            while True:
+                mid = (l + r) // 2
+
+                part2 = (len(nums1) + len(nums2)) // 2 - (mid + 1)
+                nums1_left = nums1[mid] if mid >= 0 else -float('inf')
+                nums1_right = nums1[mid + 1] if mid + 1 <= len(nums1) - 1 else float('inf')
+                nums2_left = nums2[part2 - 1] if part2 - 1 >= 0 else -float('inf')
+                nums2_right = nums2[part2] if part2<=len(nums2)-1 else float('inf')
+                #print('nums1_left=',nums1_left,'nums1_right=',nums1_right)
+                #print('nums2_left=', nums2_left, 'nums2_right=', nums2_right)
+                if max(nums1_left, nums2_left) <= min(nums1_right, nums2_right):
+                    # the correct partition is found
+                    break
+                elif nums2_left > nums1_right:
+                    l = mid + 1
                 else:
-                    return (max(shortlist_left,longlist_left)+min(shortlist_right,longlist_right))/2
-            elif shortlist_left > longlist_right:
-                r = i - 1
+                    r = mid - 1
+            if (len(nums1) + len(nums2)) % 2 == 1:
+                return min(nums1_right, nums2_right)
             else:
-                l = i + 1
-        
+                return 0.5 * (max(nums1_left, nums2_left) + min(nums1_right, nums2_right))
