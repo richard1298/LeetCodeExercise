a9e370bd17c9ce2b2a6076e57755c91607cd54af
richard1298 <richardhxw1298@gmail.com>
2022-08-01 20:48:35 -0400

    Time: 574 ms (27.48%), Space: 0B (100.00%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,8 +1,5 @@
 /* Write your T-SQL query statement below */
 
-select e.employee_id,a.team_size
-from Employee e left join
-(SELECT team_id, count(team_id) team_size
-FROM Employee e
-group by team_id) a
-on a.team_id = e.team_id
+SELECT employee_id, 
+count(team_id) over (partition by team_id) as team_size
+from Employee
