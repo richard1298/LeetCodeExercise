2040b4ce6be3506ac9c01bebf8b156ab921682de
richard1298 <richardhxw1298@gmail.com>
2022-08-01 21:07:08 -0400

    Time: 1979 ms (23.95%), Space: 0B (100.00%) - LeetHub

--- 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
+++ 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
@@ -1,3 +1,7 @@
 /* Write your T-SQL query statement below */
-select id,name from Students 
+/*select id,name from Students 
 where department_id not in (select id from Departments)
+*/
+select id,name from Students s
+where not exists
+(select id from Departments d where s.department_id = d.id)
