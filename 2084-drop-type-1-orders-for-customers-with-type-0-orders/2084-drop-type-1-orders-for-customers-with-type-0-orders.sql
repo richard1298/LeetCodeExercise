0e32ddc22bd20331136fce79d359814d918aa0b3
richard1298 <richardhxw1298@gmail.com>
2022-08-01 22:05:36 -0400

    Time: 2898 ms (5.26%), Space: 0B (100.00%) - LeetHub

--- 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-
+/*
 select * from Orders where
    (customer_id  in (select customer_id from Orders
 group by customer_id
@@ -7,4 +7,9 @@ having count(distinct order_type)=2) and order_type = 0) or
 (customer_id not in (select customer_id from Orders
 group by customer_id
 having count(distinct order_type)=2) )
+*/
+select order_id,customer_id,order_type from Orders a where order_type = 0
+union
+select order_id,customer_id,order_type from Orders b where b.customer_id not in (select customer_id from Orders where order_type = 0)
+
 
