8976df9b2a71bc5bbfdbf21c750243e6566144d9
richard1298 <richardhxw1298@gmail.com>
2022-08-10 21:24:23 -0400

    Time: 1469 ms (47.06%), Space: 0B (100.00%) - LeetHub

--- 2020-number-of-accounts-that-did-not-stream/2020-number-of-accounts-that-did-not-stream.sql
+++ 2020-number-of-accounts-that-did-not-stream/2020-number-of-accounts-that-did-not-stream.sql
@@ -1,21 +1,9 @@
-/* Write your T-SQL query statement below */
-
-with cte as
-(select s.account_id,s.start_date, s.end_date,t2.stream2021
-from Subscriptions s 
-left join
-(select account_id, (case when sum(stream2021)>=1 then 1 else 0 end) stream2021
+select count(*) as accounts_count
+from(
+select sbs.account_id,
+sum (case when stream_date between '2021-01-01' and '2021-12-31' then 1 else 0 end) as ct
 from
-(select account_id, 
-(case when stream_date between '2021-1-1' and '2021-12-31' then 1 else 0 end) stream2021
-from Streams) t
-group by account_id)t2
-on s.account_id = t2.account_id)
-
-
-select sum(case when subs2021 = 1 and stream2021 = 0 then 1 else 0 end) as accounts_count
-from
-(select account_id, 
-(case when ((start_date between '2021-1-1' and '2021-12-31') or (end_date between '2021-1-1' and '2021-12-31')) then 1 else 0 end) as subs2021,
-isnull(stream2021,0) stream2021
-from cte) t3
+Subscriptions sbs inner join
+Streams  stm on sbs.account_id = stm.account_id
+where end_date>='2021-01-01'
+group by sbs.account_id ) A where ct=0
