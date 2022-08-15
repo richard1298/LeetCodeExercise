bd1e9ed6c1462e105e0d471c03ddc307fa853b5d
richard1298 <richardhxw1298@gmail.com>
2022-08-15 19:48:56 -0400

    Time: 1666 ms (5.26%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -14,6 +14,9 @@ from cte left join Likes l
 on cte.user1_friendid = l.user_id
 where l.page_id not in (select isnull(page_id,'') from Likes where user_id = 1)*/
 
+
+/*
+
 select distinct page_id as recommended_page
 from Likes
 where user_id in
@@ -22,3 +25,20 @@ where user_id in
 else user1_id end) as friends from Friendship
 where user1_id = 1 or user2_id = 1)
 and page_id not in (select page_id from Likes where user_id =1 )
+
+*/
+
+
+select distinct page_id as recommended_page from Likes where
+user_id in (select case when user1_id = 1 then user2_id else user1_id end as user1_friend from Friendship where user1_id = 1 or user2_id=1)
+and page_id not in (select page_id from Likes where user_id =1)
+and user_id ! = 1
+
+
+
+
+
+
+
+
+
