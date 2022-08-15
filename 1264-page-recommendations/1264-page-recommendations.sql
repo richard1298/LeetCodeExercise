4391f45ea120c2976bfbd45517bb96807606c8da
richard1298 <richardhxw1298@gmail.com>
2022-08-15 08:29:28 -0400

    Time: 435 ms (92.40%), Space: 0B (100.00%) - LeetHub

---
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -0,0 +1,24 @@
+/* Write your T-SQL query statement below */
+
+/*
+with cte as
+(select 
+(case when user1_id = 1 then user2_id 
+     when user2_id = 1 then user1_id
+     end) user1_friendid 
+from Friendship
+where user1_id = 1 or user2_id = 1)
+
+select distinct(l.page_id) recommended_page
+from cte left join Likes l
+on cte.user1_friendid = l.user_id
+where l.page_id not in (select isnull(page_id,'') from Likes where user_id = 1)*/
+
+select distinct page_id as recommended_page
+from Likes
+where user_id in
+(select distinct
+(case when user1_id = 1 then user2_id 
+else user1_id end) as friends from Friendship
+where user1_id = 1 or user2_id = 1)
+and page_id not in (select page_id from Likes where user_id =1 )
